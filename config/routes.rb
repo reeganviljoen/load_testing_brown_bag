eccb0ee00d01e6242847b063e2d8e209bbcb8ada
reeganviljoen <reeganviljoen@outlook.com>
2023-08-06 17:36:31 +0200

    add: items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :items, only: [:index]
+  root to: 'items#index'
 end
