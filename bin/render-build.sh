c7ae818c44e93d5ae017e7750ebea721bd479b1b
reeganviljoen <reeganviljoen@outlook.com>
2023-08-06 18:26:27 +0200

    add: new relic instalation

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+
+curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo  NEW_RELIC_API_KEY=NRAK-9M20RY7XQPKYO8VVA9FRO84XR9M NEW_RELIC_ACCOUNT_ID=4087803 NEW_RELIC_REGION=EU /usr/local/bin/newrelic install -y
